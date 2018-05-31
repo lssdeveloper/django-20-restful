a645aa5177162a535cf5c849aa7cd448049360cc
Leandro Serra <leandro.serra.10@gmail.com>
2018-05-31 11:55:34 -0300

    Criando e modelando a app pontos turísiticos

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+
